17778d8fdf9b3eb17d37c7e3c4f2c4e70b119245
Bjirapat <jirapatlekkla@gmail.com>
2024-07-05 17:01:07 +0700

    commit3

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php echo "my repository"?>
 <?php echo "my name is jirapat"?>
+<?php echo "Hello"?>
--- menu.php
+++ menu.php
@@ -1,6 +1,7 @@
 <?php 
-    echo <h1>echo""main</h1>;
-    echo <h1>echo""product</h1>;
-    echo <h1>echo""Map</h1>;
+ echo <h1>echo"หน้าหลัก"</h1>;
+    echo <h1>echo"สินค้า"</h1>;
+    echo <h1>echo"เกี่ยวกับ"</h1>;
+    echo <h1>echo"เเผนที่"</h1>;
 
 ?>
